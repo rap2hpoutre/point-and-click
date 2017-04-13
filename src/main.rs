874ddda5c943af0b3317ba973b78b19c6b1f6fd0
rap2h <raphaelht@gmail.com>
2017-04-13 17:23:05 +0200

    hop

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,110 @@
 extern crate piston_window;
 
 use piston_window::*;
+use std::cmp;
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 200;
+const WIDTH: f64 = 320.0;
+const HEIGHT: f64 = 200.0;
+
+struct Player {
+    x: f64,
+    y: f64,
+    objective_x: f64,
+    objective_y: f64,
+    speed: f64,
+    is_moving: bool
+}
+
+impl Player {
+    fn new() -> Player {
+        Player {
+            x:  WIDTH / 2.0,
+            y:  HEIGHT / 2.0,
+            objective_x:  WIDTH / 2.0,
+            objective_y: HEIGHT / 2.0,
+            is_moving: false,
+            speed: 0.3,
+        }
+    }
+    fn init_path_to_objective(&mut self, x: f64, y: f64) {
+        self.is_moving = true;
+        self.objective_x = x;
+        self.objective_y = y;
+    }
+    fn move_to_objective(&mut self) {
+        if self.objective_x as i32 == self.x as i32 && self.objective_y as i32 == self.y as i32 {
+            self.is_moving = false;
+        } else {
+            if (self.objective_x as i32) < (self.x as i32) {
+                self.x = self.x - self.speed;
+            } else if (self.objective_x as i32) > (self.x as i32) {
+                self.x = self.x + self.speed;
+            }
+            if (self.objective_y as i32) < (self.y as i32) {
+                self.y = self.y - self.speed;
+            } else if (self.objective_y as i32) > (self.y as i32) {
+                self.y = self.y + self.speed;
+            }
+        }
+    }
+}
+
+struct Mouse {
+    x: f64,
+    y: f64
+}
+
+impl Mouse {
+    fn set_position(&mut self, x: f64, y: f64) {
+        self.x = cmp::max(cmp::min(self.x as i32 + x as i32 - WIDTH as i32 / 2, WIDTH as i32 - 1), 0) as f64;
+        self.y = cmp::max(cmp::min(self.y as i32 + y as i32 - HEIGHT as i32 / 2, HEIGHT as i32 - 1), 0) as f64;
+    }
+}
 
 fn main() {
 
     let black = [0.0, 0.0, 0.0, 1.0];
+    let grey = [0.5, 0.5, 0.5, 1.0];
     let white = [1.0, 1.0, 1.0, 1.0];
-    let mut x: f64 = 160.0;
-    let mut y: f64 = 100.0;
+
+    let mut mouse = Mouse {
+        x:  WIDTH / 2.0,
+        y:  HEIGHT / 2.0,
+    };
+
+    let mut player = Player::new();
 
     let mut window: PistonWindow =
-        WindowSettings::new("Hello star", [WIDTH, HEIGHT])
+        WindowSettings::new("Hello star", [WIDTH as u32, HEIGHT as u32])
         .exit_on_esc(true).build().unwrap();
 
     window.set_capture_cursor(true);
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
+            
             clear([1.0; 4], g);
-            rectangle(black, [0.0, 0.0, WIDTH as f64, HEIGHT as f64], c.transform, g);
-            rectangle(white, [x, y -9.0, 2.0, 20.0], c.transform, g);
-            rectangle(white, [x - 9.0, y, 20.0, 2.0], c.transform, g);
+
+            rectangle(grey, [0.0, 0.0, WIDTH, HEIGHT], c.transform, g);
+
+            rectangle(black, [player.x - 10.0, player.y - 20.0, 20.0, 20.0], c.transform, g);
+
+            rectangle(white, [mouse.x, mouse.y -9.0, 2.0, 20.0], c.transform, g);
+            rectangle(white, [mouse.x - 9.0, mouse.y, 20.0, 2.0], c.transform, g);
+
         });
 
         if let Some(button) = e.press_args() {
-            if button == Button::Mouse(MouseButton::Left) {
-                println!("PUSHED mouse button '{:?}'", button);
-            }
-        };
-
-//        if let Some(pos) = e.mouse_cursor_args() {
-//            x = pos[0] as f64;
-//            y = pos[1] as f64;
-//        }
-
+            player.init_path_to_objective(mouse.x, mouse.y);
+            println!("PUSHED mouse button Button::Mouse({:?})", button);
+        }
 
         if let Some(pos) = e.mouse_relative_args() {
-            x = x + pos[0] as f64 - 160.0;
-            y = y + pos[1] as f64 - 100.0;
-            if x >= 320.0 {
-                x = 320.0;
-            }
-            if x < 0.0 {
-                x = 0.0;
-            }
-            if y >= 200.0 {
-                y = 200.0;
-            }
-            if y < 0.0 {
-                y = 0.0;
-            }
+            mouse.set_position(pos[0], pos[1]);
+        }
+
+        if player.is_moving {
+            player.move_to_objective();
         }
 
     }
